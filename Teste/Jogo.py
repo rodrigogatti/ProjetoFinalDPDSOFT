948fd8f250febbf9ca52b80abd6458c083e35e8a
Unknown <matheussoares.augusto@hotmail.com>
2018-05-25 04:15:19 -0300

    .

--- Teste/Jogo.py
+++ Teste/Jogo.py
@@ -10,10 +10,12 @@ import pygame,sys,random
 #Definições
 pygame.mixer.init()
 #Tela
-altura=768
-largura=1024
+altura=600
+largura=800
 metade_altura=altura/2
 metade_largura=largura/2
+
+
 
 clock=pygame.time.Clock()
 fps=60
@@ -27,15 +29,19 @@ imagem_borda=pygame.image.load("borda.png")
 #inimigo
 #imagem_inimigo1=pygame.image.load("hexagono.png")
 imagem_inimigos=[pygame.image.load("hexagono.png"),pygame.image.load("circulo.png"),pygame.image.load("linhatriangulos.png"),pygame.image.load("losango.png"),pygame.image.load("quadrado.png"),pygame.image.load("retangulo.png"),pygame.image.load("triangulo.png"),]
-velocidade_inimigo=450
+velocidade_inimigo=[200,300,250,100,400,150,125]
 hitbox_inimigo=pygame.Rect(0,0,32,32)
+vida_inimigo=3
+dano_inimigo=1
+raio_desvio=50
 
 #tiro
 imagem_tiro=pygame.image.load("bala_teste.png")
 velocidade_tiro=700
 tempos_spawn_tiro=2000
-firerate_tiro=200
+firerate_tiro=150
 tiro_frente=vector(50,0)#para o tiro sair da frente da nave e nao do meio do sprite
+dano_tiro=1
 
 tamanho_tile=32
 altura_grid=altura/tamanho_tile
@@ -44,15 +50,23 @@ largura_grid=largura/tamanho_tile
 #Sons
 jogo="tema.mp3"
 GameOver="GameOver.mp3"
-som="explosao.wav"
+som_explosao=pygame.mixer.Sound("explosao.ogg")
 som_tiro=pygame.mixer.Sound("tiro.ogg")
+som_colisao=pygame.mixer.Sound("hit.ogg")
 
 #Jogador
 velocidade_jogador=550
 rotacao_jogador=300
+vidas_jogador=5
 #imagem_jogador=pygame.image.load("3.png")
 imagem_jogador=[pygame.image.load("sprite_0.png"),pygame.image.load("sprite_1.png"),pygame.image.load("sprite_2.png"),pygame.image.load("sprite_3.png"),pygame.image.load("sprite_4.png"),pygame.image.load("sprite_5.png"),pygame.image.load("sprite_6.png")]
 hitbox_jogador=pygame.Rect(0,0,32,32)
+knockback=40
+imagem_vidacheia=pygame.image.load("vida_cheia.png")
+imagem_vidavazia=pygame.image.load("vida_vazia.png")
+
+imagem_explosao=[pygame.image.load("explosao1.png"),pygame.image.load("explosao2.png"),pygame.image.load("explosao3.png"),pygame.image.load("explosao4.png"),pygame.image.load("explosao5.png"),pygame.image.load("explosao6.png"),pygame.image.load("explosao7.png"),pygame.image.load("explosao8.png"),pygame.image.load("explosao9.png"),pygame.image.load("explosao10.png"),pygame.image.load("explosao11.png"),pygame.image.load("explosao12.png"),pygame.image.load("explosao13.png"),pygame.image.load("explosao14.png"),pygame.image.load("explosao15.png"),pygame.image.load("explosao16.png"),pygame.image.load("explosao17.png"),pygame.image.load("explosao18.png"),]
+
 #--------------------------------------------------------------------------------------------------------------------
 #Classes e funções(menos o jogo principal)
 
@@ -72,6 +86,7 @@ class jogador(pygame.sprite.Sprite):
         self.posicao=vector(metade_largura,metade_altura)
         self.rotacao=0
         self.ultimo=0#tiro
+        self.vidas=vidas_jogador
         
     #Cria movimentação de acordo com as teclas
     #Adiciona rotação
@@ -104,6 +119,7 @@ class jogador(pygame.sprite.Sprite):
         if dir == 'x':
             colidiu=pygame.sprite.spritecollide(self, self.game.limitadores, False,colidiu_func)
             if colidiu:
+                som_colisao.play()
                 if self.velocidade.x > 0:
                     self.posicao.x = colidiu[0].rect.left - self.hitbox_rect.width / 2.0
                 if self.velocidade.x < 0:
@@ -113,6 +129,7 @@ class jogador(pygame.sprite.Sprite):
         if dir == 'y':
             colidiu = pygame.sprite.spritecollide(self, self.game.limitadores, False, colidiu_func)
             if colidiu:
+                som_colisao.play()
                 if self.velocidade.y > 0:
                     self.posicao.y = colidiu[0].rect.top - self.hitbox_rect.width/ 2.0
                 if self.velocidade.y < 0:
@@ -138,8 +155,10 @@ class jogador(pygame.sprite.Sprite):
         self.hitbox_rect.centery = self.posicao.y
         parede_colisao(self,self.game.limitadores,'y')
         
-        self.rect.center = self.hitbox_rect.center        
-
+        self.rect.center = self.hitbox_rect.center 
+        
+            
+        
         #surface.blit(self.image,(self.rect))  
         
 #Colisao com os limites para qualquer objeto        
@@ -169,7 +188,7 @@ def colidiu_func(objeto_1,objeto_2):
 
 #Limitador do jogo
 class Limite_fisico(pygame.sprite.Sprite):
-    def __init__(self, loopPrincipal, x, y):
+    def __init__(self, surface,loopPrincipal, x, y):
         self.groups =loopPrincipal.all_sprites, loopPrincipal.limitadores
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.game = loopPrincipal
@@ -231,6 +250,14 @@ class Inimigo(pygame.sprite.Sprite):
         self.aceleracao=vector(0,0)
         self.rect.center=self.posicao
         self.rotacao=0
+        self.vida=vida_inimigo
+        
+    def desviar_inimigos(self):
+        for enemie in self.game.inimigos:
+            if enemie != self:
+                distancia=self.posicao - enemie.posicao
+                if 0 < distancia.length() < raio_desvio:
+                    self.aceleracao=self.aceleracao+distancia.normalize()
         
     def update(self,surface):
         self.rotacao=(self.game.jogador.posicao-self.posicao).angle_to(vector(1,0))
@@ -238,7 +265,10 @@ class Inimigo(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.center=self.posicao
         
-        self.aceleracao=vector(velocidade_inimigo,0).rotate(-self.rotacao)
+        #self.aceleracao=vector(velocidade_inimigo,0).rotate(-self.rotacao)
+        self.aceleracao=vector(1,0).rotate(-self.rotacao)
+        self.desviar_inimigos()
+        self.aceleracao.scale_to_length(random.choice(velocidade_inimigo))
         self.aceleracao=self.aceleracao+self.velocidade*-1
         self.velocidade=self.velocidade+self.aceleracao*dt
         
@@ -247,10 +277,15 @@ class Inimigo(pygame.sprite.Sprite):
         parede_colisao(self,self.game.limitadores,'x')
         self.hitbox_rect.centery=self.posicao.y
         parede_colisao(self,self.game.limitadores,'y')
+        
         self.rect.center=self.hitbox_rect.center
-
+        if self.vida <= 0:
+            som_explosao.play()
+            self.kill()
+            
 #Tiro
 class Tiro(pygame.sprite.Sprite):
+    
     def __init__(self, loopPrincipal, posicao, dir):
         self.groups =loopPrincipal.all_sprites, loopPrincipal.tiros
         pygame.sprite.Sprite.__init__(self, self.groups)
@@ -273,8 +308,44 @@ class Tiro(pygame.sprite.Sprite):
         
         if pygame.time.get_ticks()-self.spawn > tempos_spawn_tiro:
             self.kill()
+            
+#Contador de vidas
+def vida_jogador(surface,x,y,vidas,imagem1,imagem2):
+    for i in range(vidas):
+        imagem1_rect=imagem1.get_rect()
+        imagem2_rect=imagem2.get_rect()
+        
+        imagem1_rect.x=x+30*i
+        imagem1_rect.y=y
+        
+        surface.blit(imagem1,imagem1_rect)
         
         
+#explosoes
+class Olha_a_explosao(pygame.sprite.Sprite):
+    def __init__(self, centro,tamanho):
+        pygame.sprite.Sprite.__init__(self)
+        self.tamanho=(45,45)
+        self.image=imagem_explosao[self.tamanho][0]
+        self.rect=self.image.get_rect
+        self.frame_inicial=0
+        self.ultimo_update=pygame.time.get_ticks()
+        self.framesPS=60
+        
+    def update(self):
+        momento=pygame.time.get_ticks()
+        if momento - self.ultimo_update > self.framesPS:
+            self.ultimo_update = momento
+            self.frame_inicial=self.frame_inicial+1
+            if self.frame_inicial == len(imagem_explosao[self.tamanho]):
+                self.kill()
+            else:
+                center=self.rect.center
+                self.image=imagem_explosao[self.size][self.frame_inicial]
+                self.rect=self.image.get_rect()
+                self.rect.center=center
+        
+#Game over
 #--------------------------------------------------------------------------------------------------------------------
 #Loop principal
 class loopPrincipal:
@@ -285,7 +356,7 @@ class loopPrincipal:
         pygame.display.set_caption("Guerra da Geometria")
         self.clock=pygame.time.Clock()
         self.map=Limite("limitacao.txt")
-        pygame.mixer.music.load(jogo)
+        pygame.mixer.music.load("jogo.mp3")
         pygame.mixer.music.play(-1)          
         
     def dar_load(self):
@@ -294,21 +365,26 @@ class loopPrincipal:
         self.inimigos=pygame.sprite.Group()
         self.tiros=pygame.sprite.Group()
         
+        lista_linhas=[]
+        lista_colunas=[]  
+        inimiigo= []
+        
         for linhas,tile_1 in enumerate(self.map.local):
             for colunas ,tile_2 in enumerate(tile_1):
                 if tile_2=="P":
-                    Limite_fisico(self,colunas,linhas)
+                    Limite_fisico(self.tela,self,colunas,linhas)
                 if tile_2=="J":
                     self.jogador=jogador(self,colunas,linhas)
                 if tile_2=="I":
-                    Inimigo(self,colunas,linhas)  
-        numero_inimigos=0
-        #for x in range(0,64):
-            #for y in range(0,29):
-                #while numero_inimigos <=6:
-                   # Inimigo(self,x,y)
-                   # numero_inimigos+=1
+                    inimiigo.append(Inimigo(self,colunas,linhas))                
+                
+                
+                lista_linhas.append(linhas)
+                lista_colunas.append(colunas)
+                
+                
         
+          
         self.camera=Cam(self.map.largura,self.map.altura)
         
     def roda(self):
@@ -327,15 +403,32 @@ class loopPrincipal:
         self.all_sprites.update(self.tela)
         self.camera.update(self.jogador)
         
+        hitou=pygame.sprite.spritecollide(self.jogador,self.inimigos,False,colidiu_func)
+        for hit in hitou:
+            som_colisao.play()
+            self.jogador.vidas=self.jogador.vidas-1
+            hit.velocidade=vector(0,0)       
+            
+            if self.jogador.vidas<= 0:
+                som_explosao.play()
+                #explosao=Olha_a_explosao(self.jogador.rect.center,jogador)
+                #all_sprites.add(explosao)
+                self.rodar=False
+                            
+                
+        if hitou:
+            self.jogador.posicao=self.jogador.posicao+ vector(knockback, 0).rotate(-hitou[0].rotacao) 
+            
         hitou=pygame.sprite.groupcollide(self.inimigos,self.tiros,False,True)
         for hit in hitou:
-            hit.kill()
-        
-        
+            hit.vida=hit.vida-dano_tiro
+            hit.velocidade=vector(0,0)
+            
     def blitar(self):
         pygame.display.set_caption("{:.2f}".format(self.clock.get_fps()))
         self.tela.fill((0,0,0))
         self.tela.blit(imagem_fundo, (0,0))
+        vida_jogador(self.tela,largura-150,5,self.jogador.vidas,imagem_vidacheia,imagem_vidavazia)
         for sprite in self.all_sprites:
             self.tela.blit(sprite.image,self.camera.apply(sprite))
         pygame.display.flip()
@@ -350,7 +443,8 @@ class loopPrincipal:
 #Função chamada pelo menu para iniciar o jogo
 #def start():
 jogo = loopPrincipal()
-while True:
-    jogo.dar_load()
-    jogo.roda()
+
+#while True:
+    #jogo.dar_load()
+    #jogo.roda()
 #--------------------------------------------------------------------------------------------------------------------
--- Teste/menu.py
+++ Teste/menu.py
@@ -7,7 +7,7 @@ Created on Sat Apr 28 17:20:02 2018
 
 import pygame,sys,time,random
 from pygame.locals import *
-import Jogo as jogo
+from Jogo import *
 
 
 def menu():
@@ -19,9 +19,10 @@ def menu():
     cursor = ("cursor.png")
     icon = pygame.image.load("icone.ico")
     pygame.display.set_icon(icon)
+    som_select=pygame.mixer.Sound("select.ogg")
     
     pygame.display.set_caption("Guerra da Geometria")
-    tela = pygame.display.set_mode((1024,768))
+    tela = pygame.display.set_mode((800,600))
     fundo=pygame.image.load('fundo.jpg')
     frames=30
     relogio=pygame.time.Clock()
@@ -45,18 +46,18 @@ def menu():
     pygame.mixer.music.load(musica1) #MÃºsica
     pygame.mixer.music.play(-1)    
     
-    
+    global markerp
     #variavel para o movimento do cursor
     mov=0
-    start=[350,305]
+    start=[250,250]
     #Loop Principal
     mainloop = True
     while mainloop:
         tela.blit(fundo, (0,0))      
         tempo=relogio.tick(frames)
-        tela.blit(js, [420,300])
-        tela.blit(tut, [410,400])
-        tela.blit(exit, [410,500])
+        tela.blit(js, [330,250])
+        tela.blit(tut, [330,350])
+        tela.blit(exit, [330,450])
         tela.blit(pygame.image.load(cursor), start)
         
        
@@ -71,55 +72,68 @@ def menu():
                 
             elif event.type==pygame.KEYDOWN:
                 if event.key==K_ESCAPE:
+                    som_select.play()
                     pygame.mixer.music.fadeout(2)
                     mainloop=False
                     
+                  
+                    
                 elif js==jogar_selecionado and event.key==K_DOWN:
+                    som_select.play()
                     js=jogar
                     tut=tutorial_selecionado
                     exit=sair
                     markerp=2
-                    start=[350,405]
+                    start=[250,350]
             
                 elif js==jogar_selecionado and event.key==K_UP:
+                    som_select.play()
                     js=jogar
                     tut=tutorial
                     exit=sair_selecionado
                     markerp=3
-                    start=[350,505]
+                    start=[250,450]
             
                 elif tut==tutorial_selecionado and event.key==K_DOWN:
+                    som_select.play()
                     js=jogar
                     tut=tutorial
                     exit=sair_selecionado
                     markerp=3
-                    start=[350,505]
+                    start=[250,450]
             
                 elif tut==tutorial_selecionado and event.key==K_UP:
+                    som_select.play()
                     js=jogar_selecionado
                     tut=tutorial
                     exit=sair
                     markerp=1
-                    start=[350,305]
+                    start=[250,250]
             
                 elif exit==sair_selecionado and event.key==K_DOWN:
+                    som_select.play()
                     js=jogar_selecionado
                     tut=tutorial
                     exit=sair
                     markerp=1
-                    start=[350,305]
+                    start=[250,250]
             
                 elif exit==sair_selecionado and event.key==K_UP:
+                    som_select.play()
                     js=jogar
                     tut=tutorial_selecionado
                     exit=sair
                     markerp=2
-                    start=[350,405]
+                    start=[250,350]
             
                 elif markerp==1 and event.key==pygame.K_RETURN:
-                    jogo.start()
+                    som_select.play()
+                    jogo = loopPrincipal()
+                    jogo.dar_load()
+                    jogo.roda()                    
             
                 elif markerp==3 and event.key==pygame.K_RETURN:
+                    som_select.play()
                     pygame.mixer.music.fadeout(2)
                     sys.exit()
             
